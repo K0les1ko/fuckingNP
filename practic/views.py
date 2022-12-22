9b3fa9fb0f1f56c8f27bb81f829c4594a85e794f
K0les1ko <mujljluoh@mail.ru>
2022-12-22 14:58:49 +0300

    1

--- practic/views.py
+++ practic/views.py
@@ -13,22 +13,10 @@ class ProductsList(ListView):
     model = Post
     ordering = 'id'
     template_name = 'news.html'
-    context_object_name = 'news'
+    context_object_name = 'post'
 
     # Метод get_context_data позволяет нам изменить набор данных,
     # который будет передан в шаблон.
-    def get_context_data(self, **kwargs):
-        # С помощью super() мы обращаемся к родительским классам
-        # и вызываем у них метод get_context_data с теми же аргументами,
-        # что и были переданы нам.
-        # В ответе мы должны получить словарь.
-        context = super().get_context_data(**kwargs)
-        # К словарю добавим текущую дату в ключ 'time_now'.
-        context['time_now'] = datetime.utcnow()
-        # Добавим ещё одну пустую переменную,
-        # чтобы на её примере рассмотреть работу ещё одного фильтра.
-        context['next_sale'] = "Распродажа в среду!"
-        return context
 
     # Переопределяем функцию получения списка товаров
     def get_queryset(self):
